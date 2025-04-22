6c5d732673b3c627e828576340ada2b8827d928c
fkqlaus <fkqlaus@naver.com>
2025-04-23 08:15:55 +0900

    test commit

---
+++ src/main/java/com/example/noticetest/NoticeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeTestApplication.class, args);
+    }
+
+}
